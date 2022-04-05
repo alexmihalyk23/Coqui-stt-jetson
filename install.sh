e6c6b4becc4e7d3e392233d62013d37809692c64
Alexey Mikhailyuk <35634279+alexmihalyk23@users.noreply.github.com>
2022-04-05 23:10:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,5 +16,11 @@ pip3 install pyaudio
 pip3 install STT-1.0.0-cp36-cp36m-linux_aarch64.whl --user
 pip3 install coqui_stt_ctcdecoder-1.0.0-cp36-cp36m-linux_aarch64.whl --user
 pip3 install pyserial
+pip3 install -U scipy
+pip3 install pyaudio==0.2.11
+pip3 install webrtcvad==2.0.10
+pip3 install halo==0.0.18
+pip3 install numpy==1.15.1
+pip3 install scipy==1.1.0
 wget https://coqui.gateway.scarf.sh/russian/jemeyer/v0.1.0/wiki-ru-6gram.scorer
 unzip model.zip
