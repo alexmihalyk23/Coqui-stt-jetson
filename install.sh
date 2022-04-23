226e61d552044533353aabef9ca4f531e55be537
Alexey Mikhailyuk <35634279+alexmihalyk23@users.noreply.github.com>
2022-04-23 16:51:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ REQUIRED_PKG="$1"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
 echo Checking for $REQUIRED_PKG: $PKG_OK
 if [ "" = "$PKG_OK" ]; then
-  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  echo "Отсутствует пакет: $REQUIRED_PKG. Установка $REQUIRED_PKG."
   sudo apt-get --yes install $REQUIRED_PKG
 fi
 }
